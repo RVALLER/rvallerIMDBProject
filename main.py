01899eecb0122599649822692ea9abbfa7e313a5
Ryan Valler <rwv1293@gmail.com>
2022-01-26 18:08:27 -0500

    started the process for extracting/working with iMdB api

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import requests
 import json
 import imdb
 import psec
+import requests
 
-r = requests.get("https://imdb-api.com/", auth=('RVA113R', 'hwmq4lj0'))
-print(r.status_code)
-
-
-# Temp placeholder top pass Actions Test on GitHub
-def test_placeholder():
-    pass
+url = "https://imdb-api.com/en/API/Top250TVs/k_hwmq4lj0"
+payload = {}
+headers = {}
+response_API = requests.request("GET", url, headers=headers, data=payload)
+# print(response_API.status_code)
+data = response_API.text
+json.loads(data)
+parse_json = json.loads(data)
