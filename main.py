be4ab14c8c7425c820f0630dd3b9051e88bff1cb
Ryan Valler <rwv1293@gmail.com>
2022-01-30 09:07:24 -0500

    Fixed code to now output the top 250 in a neat and orderly format.

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 # Start to my IMDB Project
-import json
+import csv
 import sys
 import requests
-import numpy as np
-import pandas as pd
 import secret
+from bs4 import BeautifulSoup
+import _pickle as cPickle
+import json
+import pandas as pd
 
 
 def main():
-    #Save Reference
+    # Save Reference
     original_stdout = sys.stdout
     url = f"https://imdb-api.com/en/API/Top250TVs/{secret.main()}"
     results = requests.get(url)
+
     if results.status_code != 200:
         print("Uh-Oh")
         return
     data_pull = results.json()
-    print(data_pull)
-    with open("optoptv.txt", 'w') as f:
-        for key, value in data_pull.items():
-            f.write('%s%s\n\n' % (key, value))
-    f.close()
-
+   # print(str(data_pull))
+    thelist=data_pull['items']
+    # print(list)
+    keys = thelist[0].keys()
+    with open("output.csv", 'w') as f:
+        dict_writer = csv.DictWriter(f, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(thelist)
+        f.close()
 
 
 main()
-
-
-
-
