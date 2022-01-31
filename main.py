3f83cfa4ca5b8fffe3ead3a5f7d9ba05c7433163
Ryan Valler <rwv1293@gmail.com>
2022-01-31 15:03:30 -0500

    updated with functional first two parts of sprint 1

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import csv
 import sys
 import requests
 import secret
-from bs4 import BeautifulSoup
-import _pickle as cPickle
-import json
+from time import sleep
+from urllib.request import Request, urlopen
 import pandas as pd
 
 
 def main():
-    # Save Reference
+    # ------------------------------ Part I Start ------------------------------- #
     original_stdout = sys.stdout
     url = f"https://imdb-api.com/en/API/Top250TVs/{secret.main()}"
     results = requests.get(url)
@@ -19,8 +18,8 @@ def main():
         print("Uh-Oh")
         return
     data_pull = results.json()
-   # print(str(data_pull))
-    thelist=data_pull['items']
+    # print(str(data_pull))
+    thelist = data_pull['items']
     # print(list)
     keys = thelist[0].keys()
     with open("output.csv", 'w') as f:
@@ -28,6 +27,21 @@ def main():
         dict_writer.writeheader()
         dict_writer.writerows(thelist)
         f.close()
+
+    # -------------------------------- PART 2 -------------------------------------- #
+
+    loc = f"https://imdb-api.com/en/API/UserRatings/{secret.main()}/tt7462410"
+    res_2 = requests.get(loc)
+    data_2 = res_2.json()
+    form = pd.DataFrame.from_dict(data_2)
+    # print(data_2)
+    with open("output2.csv",'w') as csv_file:
+        form.to_csv(csv_file)
+    # csv_columns = ['imDbID','title', 'fullTitle','type', 'year', 'totalRating', 'totalRatingVotes', 'ratings',]
+
+
+
+
 
 
 main()
