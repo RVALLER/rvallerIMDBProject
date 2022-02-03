0c19b22944c4f48630ba0b29e0796e6f6541e945
Ryan Valler <82516538+Demigas@users.noreply.github.com>
2022-02-03 10:23:29 -0500

    added requirements.txt

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # Start to my IMDB Project
 import csv
 import requests
-import secret
+import secrets
 import pandas as pd
 
 
 def main():
     # ------------------------------ Part I Start ------------------------------- #
-    url = f"https://imdb-api.com/en/API/Top250TVs/{secret.main()}"
+    url = f"https://imdb-api.com/en/API/Top250TVs/{secrets.main()}"
     results = requests.get(url)
 
     if results.status_code != 200:
@@ -23,7 +23,7 @@ def main():
         f.close()
     # -------------------------------- PART 2 -------------------------------------- #
 
-    loc = f"https://imdb-api.com/en/API/UserRatings/{secret.main()}/tt7462410"
+    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.main()}/tt7462410"
     res_2 = requests.get(loc)
     data_2 = res_2.json()
     form = pd.DataFrame.from_dict(data_2)  # Creates a dataframe in pandas to extract a single api scrape for csv output
@@ -36,19 +36,19 @@ def main():
     # top1, top50, top100 and top200 are variables to store api data for ratings
     # forms01-04 are for dataframing the apis and scrapes01-04 are simply to fetch request the data
 
-    top1 = f"https://imdb-api.com/en/API/UserRatings/{secret.main()}/tt5491994"
+    top1 = f"https://imdb-api.com/en/API/UserRatings/{secrets.main()}/tt5491994"
     scrape = requests.get(top1)
     data01 = scrape.json()
     form01 = pd.DataFrame.from_dict(data01)
-    top50 = f"https://imdb-api.com/en/API/UserRatings/{secret.main()}/tt0081834"
+    top50 = f"https://imdb-api.com/en/API/UserRatings/{secrets.main()}/tt0081834"
     scrape02 = requests.get(top50)
     data02 = scrape02.json()
     form02 = pd.DataFrame.from_dict(data02)
-    top100 = f"https://imdb-api.com/en/API/UserRatings/{secret.main()}/tt4786824"
+    top100 = f"https://imdb-api.com/en/API/UserRatings/{secrets.main()}/tt4786824"
     scrape03 = requests.get(top100)
     data03 = scrape03.json()
     form03 = pd.DataFrame.from_dict(data03)
-    top200 = f"https://imdb-api.com/en/API/UserRatings/{secret.main()}/tt1492966"
+    top200 = f"https://imdb-api.com/en/API/UserRatings/{secrets.main()}/tt1492966"
     scrape04 = requests.get(top200)
     data04 = scrape04.json()
     form04 = pd.DataFrame.from_dict(data04)
