4ec2a19a05219c6789850efad19b639b78553ff7
Ryan Valler <rwv1293@gmail.com>
2022-02-10 14:23:05 -0500

    final attempt for now maybe? flake8

--- tests/test_sprint2.py
+++ tests/test_sprint2.py
@@ -8,10 +8,6 @@ import sqlite3
 def test_correct_retrieval():
     url = f"https://imdb-api.com/en/API/Top250TVs/{secrets.secret_key}"
     results = requests.get(url)
-
-    if results.status_code != 200:
-        print("Uh-Oh")
-        return
     data_pull = results.json()
     thelist = data_pull['items']  # Takes only the pertinent info from the data scrape and appends to dictionary
     assert len(thelist) == 250
