d5b70dc4e93e6969fbb26f05d8ea26e5ec176c47
Ryan Valler <rwv1293@gmail.com>
2022-02-28 19:08:54 -0500

    Changelog:   Continued Work on movie tables overlap ***extremely stuck
    To Do:
      Figure out why overlap methods aren't storing any data into the table.

--- gui_main.py
+++ gui_main.py
@@ -1,7 +1,9 @@
 import sqlite3
 import sys
-from PyQt5.QtWidgets import QDialog, QMessageBox, QApplication, QStackedWidget
+from PyQt5.QtWidgets import QDialog, QMessageBox, QApplication, QStackedWidget, QTableWidgetItem, QTableView, \
+    QListWidget, QListWidgetItem
 from PyQt5.uic import loadUi
+
 from Sprint_3 import pop_all, empty_all, open_db, close_db
 
 
@@ -26,31 +28,63 @@ class mainwin(QDialog):
         pop_all()
 
 
-class Mov_overlap(QDialog):
-    def __init__(self):
-        super(Mov_overlap, self).__init__()
-
-        self.topop_mov.clicked.connect(self.goto_movOverlap)
-        loadUi("top_movOverlap.ui", self)
-        name = 'movie_api.db'
-        conn, cursor = open_db(name)
-        cursor.execute('''SELECT
-        fullTitle,rating,rating_count,rankUpDown FROM
-        movie_headlines h
-        INNER JOIN pop_movies p
-        ON h.id = p.id''')
-        self.mov_overlaps.setRowCount(0)
-
-    def goto_movOverlap(self):
-        m_over = Mov_overlap()
-        widget.addWidget(m_over)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
-
-
 class vizdata(QDialog):
     def __init__(self):
         super(vizdata, self).__init__()
         loadUi("data_vizMain.ui", self)
+        self.back.clicked.connect(self.CancelButton)
+        self.topop_mov.clicked.connect(self.goTopPopOv)
+
+    def goTopPopOv(self):
+        popMovOv = pop250MovOlap()
+        widget.addWidget(popMovOv)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def CancelButton(self):
+        newWind = mainwin()
+        widget.addWidget(newWind)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class pop250MovOlap(QDialog):
+    def __init__(self):
+        super(pop250MovOlap, self).__init__()
+        name = 'movie_api.db'
+        open_db(name)
+        loadUi("top_movOverlap.ui", self)
+        self.back.clicked.connect(self.CancelButton)
+        self.get_overlap()
+
+    def CancelButton(self):
+        newWind = vizdata()
+        widget.addWidget(newWind)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def get_overlap(self):
+        name = "movie_api.db"
+        conn, curs = open_db(name)
+        curs.execute(f"""SELECT t.full_title, t.rating, t.rating_count, p.rankUpDown
+                             FROM movie_headlines t
+                             INNER JOIN pop_movies p 
+                             ON t.id = p.id""")
+        records = curs.fetchall()
+        to_dict(records)
+        self.put_data_in_list(self.records)
+
+
+def put_data_in_list(self, data):
+    numrows = len(data)
+    numcols = len(data[0])
+    self.movo.setColumnCount(numcols)
+    self.movo.setRowCount(numrows)
+    for row in range(numrows):
+        for column in range(numcols):
+            self.movo.setItem(row, column, QTableWidgetItem((data[row][column])))
+
+
+def to_dict(lst_tuples):
+    result = [dict(el) for el in lst_tuples]
+    return result
 
 
 # main
